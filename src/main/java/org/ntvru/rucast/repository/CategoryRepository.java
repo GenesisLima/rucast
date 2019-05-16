d3e6847177a75fe78d4d5356e3a78c689f0f187f
Prof. Genesis Lima <profgenesislima@gmail.com>
2019-05-16 10:22:58 -0300

    CategoryRepository class added.

--- src/main/java/org/ntvru/rucast/repository/CategoryRepository.java
+++ src/main/java/org/ntvru/rucast/repository/CategoryRepository.java
@@ -9,6 +9,7 @@ import org.ntvru.rucast.model.Category;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Transactional
 public interface CategoryRepository extends CrudRepository<Category, Long> {
